4734446ba18c5a63a6c0c89765e22705d8f1effc
Louie Richardson <lrich99@protonmail.com>
2021-04-28 17:37:43 +0100

    Removed unused modal window on Phantom of the Opera work page

--- pages/work/phantom-of-the-opera.js
+++ pages/work/phantom-of-the-opera.js
@@ -1,27 +1,10 @@
 import { useState } from "react";
 import Header from "../../components/Header";
-import Modal from "../../components/Modal";
 import Grid from "../../components/Grid";
 import { client } from "../../utils/prismicClient";
 import Meta from "../../components/Meta";
 
 export default function workContent({ workImages, work }) {
-  const [isModalOpen, setModalIsOpen] = useState(false);
-  const [imageName, setImageName] = useState("");
-  const [imageURL, setImageURL] = useState("");
-
-  const handleModalOpen = (image) => {
-    setModalIsOpen(true);
-    setImageName(image.work_image.alt);
-    setImageURL(image.work_image.url);
-  };
-
-  const handleModalClose = () => {
-    setModalIsOpen(false);
-    setImageName("");
-    setImageURL("");
-  };
-
   return (
     <>
       <Meta title={work.data.work_name[0].text} />
@@ -32,25 +15,7 @@ export default function workContent({ workImages, work }) {
             {work.data.work_name[0].text}
           </h1>
         )}
-        {workImages && (
-          <Grid images={workImages} handleModalOpen={handleModalOpen} />
-        )}
-        <div
-          onClick={isModalOpen ? handleModalClose : null}
-          className={
-            isModalOpen
-              ? "transition ease-in-out duration-500 flex bg-gray-600 bg-opacity-75 w-full h-full top-0 left-0 fixed items-center justify-center"
-              : null
-          }
-        >
-          {isModalOpen && (
-            <Modal
-              handleModalClose={handleModalClose}
-              imageName={imageName}
-              imageURL={imageURL}
-            />
-          )}
-        </div>
+        {workImages && <Grid images={workImages} />}
       </main>
     </>
   );
